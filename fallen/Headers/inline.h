0b1015bc5e81724224458b1834e53bc2f7ee602b
mpod <mpodbiera@gmail.com>
2020-08-14 22:34:59 +0200

    Auto stash before rebase of "refs/heads/cmake"
    replaced with c++ code

--- fallen/Headers/inline.h
+++ fallen/Headers/inline.h
@@ -28,35 +28,39 @@ SLONG MUL64(SLONG, SLONG);
 #else
 
 inline SLONG	DIV64(SLONG a,SLONG b)
-{
+{ //TODO make asm code usable again
 /* 64bit divide */        
-	SLONG		ret_v;
-	__asm
-	{                       
-		 mov	edx,a
-		 mov	ebx,b
-		 mov	eax,edx
-		 shl	eax,16			
-		 sar	edx,16			
-		 idiv	ebx
-		 mov	ret_v,eax
-	}
-	return(ret_v);
+	// SLONG		ret_v;
+	// __asm
+	// {                       
+	// 	 mov	edx,a
+	// 	 mov	ebx,b
+	// 	 mov	eax,edx
+	// 	 shl	eax,16			
+	// 	 sar	edx,16			
+	// 	 idiv	ebx
+	// 	 mov	ret_v,eax
+	// }
+	// return(ret_v);
+	SLONG result = a/b;
+	return result;
 }
 
 inline SLONG	MUL64(SLONG a,SLONG b)
-{
+{ //TODO make asm code usable again
 /* 64bit multiply */        
-	SLONG		ret_v;
-	__asm
-	{                       
-		 mov	eax,a       
-		 imul   b			
-		 mov	ax,dx       
-		 rol	eax,16
-		 mov	ret_v,eax
-	}
-	return(ret_v);
+	// SLONG		ret_v;
+	// __asm
+	// {                       
+	// 	 mov	eax,a       
+	// 	 imul   b			
+	// 	 mov	ax,dx       
+	// 	 rol	eax,16
+	// 	 mov	ret_v,eax
+	// }
+	// return(ret_v);
+	SLONG result = a*b;
+	return result;
 }
 #endif
 #else
